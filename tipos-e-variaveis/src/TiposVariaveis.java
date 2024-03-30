a2c4238c81fd0d7c10b11b57c56377bc666ea9d6
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-03-30 15:33:52 -0300

    variáveis e constantes em java

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // uma variável pode ser declarada com um valor e ter esse mesmo valor alterado, tendo apenas que respeitar o tipo
+        int numero = 10;
+        numero = 5;
+        System.out.println(numero);
+
+        // para declarar variáveis constantes, segue-se a seguinte prática: final <tipo> nome (caixa alta)
+        final double valor_Pi = 3.14;
+        System.out.println(valor_Pi);
     }
 }
