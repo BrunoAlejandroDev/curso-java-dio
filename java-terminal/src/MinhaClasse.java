103598ca024af54b50e3c8625e2f7bd115ce8481
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-04-15 10:57:43 -0300

    navegando entre diretórios e executando via prompt

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,6 @@
+public class MinhaClasse {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Oi, eu fui executado pelo terminal");
+        System.out.println("Eba, o Fortaleza ganhou");
+    }
+}
