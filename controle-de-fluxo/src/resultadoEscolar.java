b32f71b462512e427fd48a30efd57ccfde91d1b5
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-04-22 10:58:12 -0300

    exemplo 2 de estrutura condicional

---
+++ controle-de-fluxo/src/resultadoEscolar.java
@@ -0,0 +1,15 @@
+public class resultadoEscolar {
+    public static void main(String[] args) {
+        double notaAluno = 7.2;
+
+        if (notaAluno >= 7) {
+            System.out.println("Aprovado");
+        }
+        else if (notaAluno >= 5 && notaAluno < 7) {
+            System.out.println("Recuperação");
+        }
+        else {
+            System.out.println("Reprovado");
+        }
+    }
+}
