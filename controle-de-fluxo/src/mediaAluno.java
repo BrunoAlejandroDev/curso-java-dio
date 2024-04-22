d7dc5c4f06e506f9604e39eb2b757a8e3805e502
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-04-22 11:03:55 -0300

    operador ternário

---
+++ controle-de-fluxo/src/mediaAluno.java
@@ -0,0 +1,7 @@
+public class mediaAluno {
+    public static void main(String[] args) {
+        double nota = 4.9;
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
+    }
+}
