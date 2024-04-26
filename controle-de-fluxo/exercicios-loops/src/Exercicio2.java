58435f75c939438af0c097726c5103ed7bbfcbec
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-04-26 15:46:47 -0300

    loop - ex2 - imprimir pares 1 a 20

---
+++ controle-de-fluxo/exercicios-loops/src/Exercicio2.java
@@ -0,0 +1,21 @@
+/*
+    Escreva um programa que use um loop para imprimir todos os números pares de 1 a 20.
+*/
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+        int num = 1;
+        while (num <= 20) {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+}
