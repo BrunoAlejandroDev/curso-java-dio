b2d2ef256c4ff8287d56c270391365189199b723
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-04-26 15:40:17 -0300

    loops - ex1 - imprimir num de 1 a 10

---
+++ controle-de-fluxo/exercicios-loops/src/Exercicio1.java
@@ -0,0 +1,11 @@
+/*
+    Escreva um programa que use um loop para imprimir os números de 1 a 10, cada um em uma linha separada.
+*/
+
+public class Exercicio1 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
