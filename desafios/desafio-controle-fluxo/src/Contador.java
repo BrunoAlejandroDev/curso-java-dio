08ecd02b8e5e83029e79902315fe4da7b579b095
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-05-13 11:58:46 -0300

    dcf: código de resolução do desafio controle fluxo

--- desafios/desafio-controle-fluxo/src/Contador.java
+++ desafios/desafio-controle-fluxo/src/Contador.java
@@ -1,5 +1,37 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) throws Exception {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro parâmetro: ");
+        int parametroUm = sc.nextInt();
+
+        System.out.print("Digite o segundo parâmetro: ");
+        int parametroDois = sc.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+        }
+        catch (ParametrosInvalidosException exception){
+            exception.printStackTrace();
+            System.out.println("ERRO: O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        sc.close();
+    }
+
+    public static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
     }
 }
